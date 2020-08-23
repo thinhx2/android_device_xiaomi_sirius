136d22ccd0c7809c229d1f57b8e3e925cfea1c42
guoxiao1213 <598464949@qq.com>
2020-08-23 19:04:31 +0800

    sirius :sepolicy: Allow platform_app to read sysfs battery and kgsl

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,5 +34,8 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
+# Sepolicy
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+
 # Inherit from the proprietary stuffs
 -include vendor/xiaomi/sirius/BoardConfigVendor.mk
