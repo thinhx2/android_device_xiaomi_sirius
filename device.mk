13c194563eba8859eec543250f4e496ba6850177
guoxiao1213 <598464949@qq.com>
2020-09-17 18:57:15 +0800

    sirius :Merge gps_debug.conf into gps.conf(Part1)

--- device.mk
+++ device.mk
@@ -60,10 +60,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.target.rc
 
-# GPS
-PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/gps/gps_debug.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/gps_debug.conf
-
 # Input
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/idc/uinput-fpc.idc:$(TARGET_COPY_OUT_SYSTEM)/usr/idc/uinput-fpc.idc \
